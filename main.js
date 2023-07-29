ad36c187d429a1156fed43246370872c596f97d1
Eroje <93765911+Eroje@users.noreply.github.com>
2023-07-29 15:00:31 +0100

    New Commit

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ class HospitalEmployee { // Parent Class.
     this._remainingVacationDays = 20; // Instance
   }
   
+  static generatePassword() { // Static Method (Its not associated with HospitalEmplyee Instances, it is called directly)
+    return Math.floor(Math.random() * 10001);
+  } // we can only access generatePassword() by appending it to the HospitalEmployee class as shown below
+
   get name() {
     return this._name;
   } // Getter
@@ -42,6 +46,29 @@ console.log(nurseOlynyk.remainingVacationDays);
 
 nurseOlynyk.addCertification('Genetics'); // This Method calls the addCertifcation on nurseOlynyk.
 console.log(nurseOlynyk.certifications);
+
+class Doctor extends HospitalEmployee {
+  constructor(name, insurance) {
+    super(name);
+    this._insurance = insurance;
+  } 
+  
+  get insurance() {
+    return this._insurance;
+  }
+  
+  addinsurance(newInsurance) {
+    this.insurance.push(newInsurance);
+  }
+}
+
+const docIbrahim = new Doctor('Ibrahim', 'Stanbic IBTC');
+docIbrahim.takeVacationDays(10);
+
+console.log(docIbrahim.remainingVacationDays);
+console.log(docIbrahim.insurance);
+
+console.log(HospitalEmployee.generatePassword()) // This generates password 
 
 
 
